0bd561a2e4f3db496303c4c727d49385fb4f9338
voigtito <voigt.gustavoh@gmail.com>
2022-04-05 13:51:35 -0300

    Adicionando javascript

---
+++ index.js
@@ -0,0 +1,8 @@
+function teste() {
+
+  const name = document.getElementById('name').value
+  const mail = document.getElementById('mail').value
+  const area = document.getElementById('area').value
+
+  return alert(`Woooooooow 😎 \n \n Seja bem-vindo ${name} ao bootcamp da àrea de ${area}. \n\n Você se cadastrou com o email \n ${mail}. `)
+}
